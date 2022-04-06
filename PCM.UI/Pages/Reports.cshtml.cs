3bf6d29377ac16560cd0ea8565d17e8f35e5642a
Enmanuel <emr05690@gmail.com>
2022-04-06 12:08:50 -0400

    Continue report proccess

--- PCM.UI/Pages/Reports.cshtml.cs
+++ PCM.UI/Pages/Reports.cshtml.cs
@@ -15,6 +15,7 @@ namespace PCM.UI.Pages
     {
         EntityServices entityServices = new EntityServices();
         PaymentServices paymentServices = new PaymentServices();
+        PatientServices patientServices = new PatientServices();
         LogServices logServices = new LogServices();
         ARSServices ARSServices = new ARSServices();
 
@@ -22,7 +23,20 @@ namespace PCM.UI.Pages
         public DateTime Date2 { get; set; } = DateTime.Today;
         public string Insurance { get; set; }
         public List<ARS> AllARS { get; set; }
+        public List<ReportEntry> Result { get; set; }
 
+        public class ReportEntry
+        {
+            public string PatientName { get; set; }
+            public string Insurance { get; set; }
+            public string PaymentId { get; set; }
+            public decimal PaymentAmount { get; set; }
+            public int InvoiceNumber { get; set; }
+            public DateTime DateTime { get; set; }
+            public decimal InsuranceCoverage { get; set; }
+            public decimal ApmtPrice { get; set; }
+
+        }
 
 
         public void OnGet()
@@ -32,6 +46,14 @@ namespace PCM.UI.Pages
 
             logServices.Log(string.Format("User {0} accessed the report page",User.Identity.Name));
 
+        }
+
+        public void OnPost() 
+        {
+        
+        
+        
+        
         }
     }
 }
